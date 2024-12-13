8fc2e63ea400fc19caded94f37c7d08acb3c607d
blurdel <5648737+blurdel@users.noreply.github.com>
2024-12-13 11:28:31 -0700

    Added liveness check

--- src/main/java/com/blurdel/hello/controllers/HelloController.java
+++ src/main/java/com/blurdel/hello/controllers/HelloController.java
@@ -5,6 +5,8 @@ import java.util.concurrent.atomic.AtomicLong;
 import com.blurdel.hello.model.Greeting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,6 +33,11 @@ public class HelloController {
     @GetMapping()
     public Greeting getHello(@RequestParam(value = "name", defaultValue = "World!") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("health")
+    public ResponseEntity<?> getHealth() {
+        return new ResponseEntity<>("Ok", HttpStatus.OK);
     }
 
 }
