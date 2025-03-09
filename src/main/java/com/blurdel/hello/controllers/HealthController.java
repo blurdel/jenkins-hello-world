99fc3e704677c00ef5246b3981a05d73a6d2db1b
blurdel <5648737+blurdel@users.noreply.github.com>
2025-03-09 12:28:32 -0700

    Moved health end-point into its own controller

---
+++ src/main/java/com/blurdel/hello/controllers/HealthController.java
@@ -0,0 +1,21 @@
+package com.blurdel.hello.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<?> getHealth() {
+        log.debug("/health");
+        return new ResponseEntity<>("Ok :-)", HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/blurdel/hello/controllers/HelloController.java
+++ src/main/java/com/blurdel/hello/controllers/HelloController.java
@@ -3,10 +3,7 @@ package com.blurdel.hello.controllers;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.blurdel.hello.model.Greeting;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 // http://localhost:8080/hello
 // http://localhost:8080/hello?name=Zoey
 
+@Slf4j
 @RestController
 @RequestMapping({"/", "/hello"})
 public class HelloController {
 
-    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
@@ -32,12 +29,8 @@ public class HelloController {
 
     @GetMapping()
     public Greeting getHello(@RequestParam(value = "name", defaultValue = "World!") String name) {
+        log.debug("/hello {}", String.format(template, name));
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
-
-    @GetMapping("health")
-    public ResponseEntity<?> getHealth() {
-        return new ResponseEntity<>("Ok", HttpStatus.OK);
     }
 
 }
